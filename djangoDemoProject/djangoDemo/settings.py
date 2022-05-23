256aef07d4773468cd74ceaebf87f93463a26a34
Alessandro Beggiato <g4briel.g4briel@gmail.com>
2022-05-23 19:28:41 +0200

    installed polls app

--- djangoDemoProject/djangoDemo/settings.py
+++ djangoDemoProject/djangoDemo/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'polls.apps.PollsConfig',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
